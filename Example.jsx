2c6019197c5da4c1e7bf2292dfb123011644aae9
humdrum <jjrajani@gmail.com>
2017-07-05 11:27:43 -0400

    example generated file. update readme command

---
+++ Example.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ExampleStore from './ExampleStore';
+
+export default class Example extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className='example'>
+        example componenet created!!
+      </div>
+    );
+  }
+
+}
---
+++ ExampleStore.jsx
@@ -0,0 +1,5 @@
+export default class ExampleStore {
+
+  state = {};
+
+}
